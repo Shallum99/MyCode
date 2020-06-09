04dd26c0bd1ce7b4a7c2a164262ce1b10ebabae6
Shallum99 <44576280+Shallum99@users.noreply.github.com>
2020-06-09 19:03:20 +0530

    Update PythonAsses.py

--- PythonAsses.py
+++ PythonAsses.py
@@ -1,3 +1,12 @@
+# Question:
+# To take an input(string) from the user and swap the case of that string and return the reverse of that string.
+# Sample case 0:
+# Input:
+# rUn DoGs
+# Output:
+# dOgS RuN
+
+
 ok = input().split()
 li = []
 for i in ok:
